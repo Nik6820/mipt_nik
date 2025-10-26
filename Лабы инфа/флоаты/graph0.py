dd3631feca80515461f533af50cbed117875d4c0
Nik6820 <[ngubarev@internet.ru]>
2025-10-26 12:46:32 +0300

    float laba update

--- Лабы инфа/флоаты/graph0.py
+++ Лабы инфа/флоаты/graph0.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 file = open('test.csv', 'r')
-t1=[0]*100
-t2=[0]*100
-t3=[0]*100
-n=[0]*100
-for j in range(0, 100):
+N=2500
+t1=[0]*N
+t2=[0]*N
+t3=[0]*N
+n=[0]*N
+for j in range(0, N):
     inp=file.readline()
     t1[j], t2[j], t3[j] = map(float, inp.split())
     n[j]=j+1
@@ -19,4 +20,7 @@ plt.plot(n, t3, color='purple', label="right S", linewidth=1)
 plt.xlabel("Количество разбиений")##подпись оси абсцисс
 plt.ylabel("Значение интеграла")##подпись оси ординат
 plt.legend()
+plt.grid(True, which='major', linestyle='-')#мажорная сетка
+plt.grid(True, which='minor', linestyle='--', linewidth=0.5)#минорная сетка
+plt.minorticks_on()#обязательная функция для отображения минорной сетки
 plt.show()
--- Лабы инфа/флоаты/laba_float.cpp
+++ Лабы инфа/флоаты/laba_float.cpp
@@ -5,18 +5,6 @@
 
 using namespace std;
 
-int rand_uns(int min, int max)
-{
-    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    static std::default_random_engine e(seed);
-    std::uniform_int_distribution<int> d(min, max);
-    return d(e);
-}
-
-double get_time()
-{
-    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() / 1e6;
-}
 
 void binary_vision(unsigned int a)
 {
@@ -40,9 +28,12 @@ union F
 } f1;
 int main()
 {
+    cout << fixed;
+    cout.precision(10);
     ofstream ff("test.csv", ios::out);
-    f1.f = 16500000.0;
-    /* //для второго задания
+    f1.f = 1.0;
+    /*
+     //для второго задания
     for(int i=1; i<40; i++)
     {
         f1.f*=10;
@@ -50,15 +41,17 @@ int main()
         binary_vision(f1.i);
     }
     */
-    /* //третье задание
+    /*
+     //третье задание
     f1.f = 16500000.0;
     while((f1.f+1)-f1.f>=1){ //16777216.00-16777215.00=0
         cout<<f1.f<<endl;
         f1.f++;
     }
     */
+
     double lr=0.0, mr=0.0, rr=0.0;
-    for (int j=1; j<=100; j++)
+    for (int j=1; j<=2500; j++)
     {
         double lr=0.0, mr=0.0, rr=0.0;
         for(int i=1; i<=j; i++)
@@ -70,10 +63,12 @@ int main()
         ff << lr/j/j/j << " ";
         ff << mr/j/j/j << " ";
         ff << rr/j/j/j << " ";
-        ff << endl;/*
+        ff << endl;
+
+        cout<<j<<endl;
         cout<<lr/j/j/j<<endl;
         cout<<mr/j/j/j<<endl;
         cout<<rr/j/j/j<<endl;
-        cout<<endl;*/
+        cout<<endl;
     }
 }
